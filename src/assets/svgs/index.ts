bf7e1735d619b2efcffd91d75b4676b486485f48
Han Byul <yahb9@naver.com>
2022-08-03 16:53:08 +0900

    Init: 초기 설정

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,7 @@
+export { ReactComponent as SearchIcon } from './search.svg'
+export { ReactComponent as HomeIcon } from './home.svg'
+export { ReactComponent as HamburgerIcon } from './hamburger.svg'
+export { ReactComponent as ChartIcon } from './pie-chart.svg'
+export { ReactComponent as LogoIcon } from './logo.svg'
+export { ReactComponent as TimeIcon } from './access_time.svg'
+export { ReactComponent as XIcon } from './x.svg'
