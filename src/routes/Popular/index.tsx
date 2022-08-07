26ada009538c8a5ef57425a211fb4114d82dcac0
Han Byul <yahb9@naver.com>
2022-08-07 23:27:55 +0900

    Feat: Cocktail api data 타입 지정

--- src/routes/Popular/index.tsx
+++ src/routes/Popular/index.tsx
@@ -2,21 +2,31 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 // import Slider from 'react-slick'
 import { useQuery } from 'react-query'
+import { mapQueryStatusFilter } from 'react-query/types/core/utils'
 
 import { popularCocktailApi } from 'services/getData'
+import { ICocktailData } from 'types/types'
 
 import styles from './popular.module.scss'
 
 const Popular = () => {
+  const [cocktailData, setCocktailData] = useState<ICocktailData[]>()
   /* const { data, isFetching } = useQuery('popularCocktailApi', popularCocktailApi, {
     onSuccess: (res) => {
-      console.log(res.data)
+      setCocktailData(data)
     },
-  }) */
-
+  })
+*/
   return (
     <div className={styles.background}>
-      <div className={styles.container} />
+      {cocktailData?.map((datas) => {
+        return (
+          <div key={datas.idDrink} className={styles.container}>
+            <img src={datas.strDrinkThumb} alt='cocktail-img' />
+            <div>dd</div>
+          </div>
+        )
+      })}
     </div>
   )
 }
---
+++ src/types/types.d.ts
@@ -0,0 +1,53 @@
+export interface ICocktailData {
+  dateModified: Date
+  idDrink: null | string
+  strAlcoholic: null | string
+  strCategory: null | string
+  strCreativeCommonsConfirmed: null | string
+  strDrink: null | string
+  strDrinkAlternate: null | string
+  strDrinkThumb: string
+  strGlass: null | string
+  strIBA: null | string
+  strImageAttribution: null | string
+  strImageSource: null | string
+  strIngredient1: null | string
+  strIngredient2: null | string
+  strIngredient3: null | string
+  strIngredient4: null | string
+  strIngredient5: null | string
+  strIngredient6: null | string
+  strIngredient7: null | string
+  strIngredient8: null | string
+  strIngredient9: null | string
+  strIngredient10: null | string
+  strIngredient11: null | string
+  strIngredient12: null | string
+  strIngredient13: null | string
+  strIngredient14: null | string
+  strIngredient15: null | string
+  strInstructions: null | string
+  strInstructionsDE: null | string
+  strInstructionsES: null | string
+  strInstructionsFR: null | string
+  strInstructionsIT: null | string
+  'strInstructionsZH-HANS': null | string
+  'strInstructionsZH-HANT': null | string
+  strMeasure1: null | string
+  strMeasure2: null | string
+  strMeasure3: null | string
+  strMeasure4: null | string
+  strMeasure5: null | string
+  strMeasure6: null | string
+  strMeasure7: null | string
+  strMeasure8: null | string
+  strMeasure9: null | string
+  strMeasure10: null | string
+  strMeasure11: null | string
+  strMeasure12: null | string
+  strMeasure13: null | string
+  strMeasure14: null | string
+  strMeasure15: null | string
+  strTags: null | string
+  strVideo: null | string
+}
