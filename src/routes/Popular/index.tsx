19bc0cbc864310296fb3d65f9e3a8b474e442aad
Han Byul <yahb9@naver.com>
2022-08-07 22:11:12 +0900

    Design: 반투명 효과 적용

--- src/routes/Popular/index.tsx
+++ src/routes/Popular/index.tsx
@@ -1,7 +1,11 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+// import Slider from 'react-slick'
 import { useQuery } from 'react-query'
+
 import { popularCocktailApi } from 'services/getData'
+
+import styles from './popular.module.scss'
 
 const Popular = () => {
   /* const { data, isFetching } = useQuery('popularCocktailApi', popularCocktailApi, {
@@ -10,7 +14,11 @@ const Popular = () => {
     },
   }) */
 
-  return <div>TopRated</div>
+  return (
+    <div className={styles.background}>
+      <div className={styles.container} />
+    </div>
+  )
 }
 
 export default Popular
