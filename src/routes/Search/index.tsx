e08b316fd409be048450d83c76c18615343df456
Han Byul <yahb9@naver.com>
2022-08-09 15:38:36 +0900

    Feat: 여러 cocktail Api 객체로 정리

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,5 +1,27 @@
+import { FormEvent, useState } from 'react'
+
+import styles from './search.module.scss'
+
 const Search = () => {
-  return <div>Search</div>
+  const [searchKeyword, setSearchKeyword] = useState('')
+
+  const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.currentTarget.value)
+  }
+
+  return (
+    <div className={styles.container}>
+      <header>
+        <div>COCKTAIL LIFE</div>
+        <nav>Navigation</nav>
+      </header>
+      <main>
+        <input type='search' value={searchKeyword} onChange={handleInputChange} />
+      </main>
+    </div>
+  )
 }
 
 export default Search
+
+// 레이아웃 만들기
--- src/services/getData.tsx
+++ src/services/getData.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const COCKTAIL_API_URL = 'https://the-cocktail-db.p.rapidapi.com/popular.php'
+const POPULAR_COCKTAIL_API_URL = 'https://the-cocktail-db.p.rapidapi.com/popular.php'
 const RAPID_API_KEY = '0f40b0457amsh95e66230e92672ep17b4c1jsn3a7564d6e110'
 const RAPID_API_HOST = 'the-cocktail-db.p.rapidapi.com'
 
 export const popularCocktailApi = async () => {
-  const data = await axios.get(COCKTAIL_API_URL, {
+  const data = await axios.get(POPULAR_COCKTAIL_API_URL, {
     headers: {
       'X-RapidAPI-Key': RAPID_API_KEY,
       'X-RapidAPI-Host': RAPID_API_HOST,
@@ -15,16 +15,33 @@ export const popularCocktailApi = async () => {
   return data
 }
 
-/* const basicApi = axios.create({
+const basicApi = axios.create({
   baseURL: 'www.thecocktaildb.com/api/json/v1/1',
 })
 
-export const cocktailApi = {
+export const cocktailApis = {
   searchByName: (params: string) =>
     basicApi.get('/search.php', {
       params: {
         s: params,
       },
     }),
+  filterByCategory: (params: string) =>
+    basicApi.get('/filter.php', {
+      params: {
+        c: params,
+      },
+    }),
+  filterByAlcoholic: (params: string) =>
+    basicApi.get('./filter.php', {
+      params: {
+        a: params,
+      },
+    }),
+  filterByIngredients: (params: string) =>
+    basicApi.get('./filter.php', {
+      params: {
+        i: params,
+      },
+    }),
 }
-*/
