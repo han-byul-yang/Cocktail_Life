ac8c8cc73a4f9d1d3bd60e5af234afec8e762bc8
Han Byul <yahb9@naver.com>
2022-08-20 23:39:25 +0900

    Fix: axios error 핸들링

--- src/utils/getApiData.ts
+++ src/utils/getApiData.ts
@@ -1,21 +1,12 @@
-import { useState } from 'react'
-import { AxiosResponse } from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
-import { cocktailApis } from 'services/getApis'
-
-const getApiData = async (api: (params: string) => Promise<AxiosResponse<any, any>>, params: string) => {
-  /* const [apiError, setApiError] = useState(false)
-  const [api, setApi] = useState<(params: string) => Promise<AxiosResponse<any, any>>>(() =>
-    cocktailApis.searchById('00000')
-  )
-  const [params, setParams] = useState('')
-*/
+const getApiData = async (api: (params: string) => Promise<AxiosResponse>, params: string) => {
   try {
     const { data } = await api(params)
     return data
   } catch (error) {
-    console.log(error)
-    return ''
+    if (axios.isAxiosError(error)) return error.message
+    return error
   }
 }
 
