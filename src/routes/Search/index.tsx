900771e3cb75f891f4716fd439020cc6a0a99755
Han Byul <yahb9@naver.com>
2022-08-21 00:05:45 +0900

    Feat: searchButtonClick 함수 1차 에러 핸들링

--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -4,8 +4,9 @@ import { useRecoilValue } from 'recoil'
 import getApiData from 'utils/getApiData'
 import { cocktailApis } from 'services/getApis'
 import { alcoholicList, categoryList, ingredientList } from 'store/initialData/initialListData'
+import { cocktailInitialData } from 'store/initialData/initialApiData'
 import { filteredItemAtom } from 'store/atom'
-import { IFilteredCocktailData } from 'types/types'
+import { ICocktailData, IFilteredCocktailData } from 'types/types'
 import FilterBox from './FilterBox'
 
 import styles from './search.module.scss'
@@ -13,88 +14,105 @@ import styles from './search.module.scss'
 const Search = () => {
   const filtering = useRecoilValue(filteredItemAtom)
   const [totalFilteredIdList, setTotalFilteredIdList] = useState<string[]>([''])
+  const [totalResult, setTotalResult] = useState<ICocktailData[]>([cocktailInitialData])
   const [inputKeyword, setInputKeyword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
   // const inputRef = useRef(null)
 
   const handleInputKeywordChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputKeyword(e.currentTarget.value)
   }
 
-  const cocktailDataToIdList = (resultData: { drinks: IFilteredCocktailData[] }) => {
-    return resultData.drinks.map((cocktailData: IFilteredCocktailData) => cocktailData.idDrink)
+  const cocktailDataToIdList = (resultData: { drinks: (ICocktailData | IFilteredCocktailData)[] }) => {
+    return resultData.drinks.map((cocktailData: ICocktailData | IFilteredCocktailData) => cocktailData.idDrink)
   }
 
-  const eliminateSameItem = (combinedItemList: string[], count: number) => {
+  const eliminateSameItem = (combinedItemList: (string | never)[], count: number) => {
     const itemKeyObject: any = {}
 
+    if (combinedItemList.length === 0) throw Error()
+
     combinedItemList.forEach((item) => {
-      if (itemKeyObject[item]) itemKeyObject[item] += itemKeyObject[item] + 1
+      if (itemKeyObject[item]) itemKeyObject[item] += 1
       else itemKeyObject[item] = 1
     })
 
-    const noSameItemList = Object.keys(itemKeyObject).filter((ele) => itemKeyObject[ele] > count)
-
+    const noSameItemList = Object.keys(itemKeyObject).filter((ele) => itemKeyObject[ele] === count)
     return noSameItemList
   }
 
   const handleSearchButtonClick = async () => {
-    const combinedIdLists = []
+    const combinedIdLists: (string | never)[] = []
     let filterKindCount = 0
 
     if (inputKeyword !== '') {
       filterKindCount += 1
 
-      const filteredByNameData = await getApiData(cocktailApis.searchByName, inputKeyword)
-      const filteredNameIdList = cocktailDataToIdList(filteredByNameData)
-      combinedIdLists.push(...filteredNameIdList)
+      await getApiData(cocktailApis.searchByName, inputKeyword)
+        .then((result) => cocktailDataToIdList(result))
+        .then((result2) => combinedIdLists.push(...result2))
+        .catch((error) => {
+          setErrorMessage('검색 결과가 없습니다')
+        })
     }
 
     if (filtering.alcoholic !== '') {
       filterKindCount += 1
 
-      const filteredByAlcoholicData = await getApiData(cocktailApis.filterByAlcoholic, filtering.alcoholic)
-      const filteredAlcoholicIdList = cocktailDataToIdList(filteredByAlcoholicData)
-      combinedIdLists.push(...filteredAlcoholicIdList)
+      await getApiData(cocktailApis.filterByAlcoholic, filtering.alcoholic)
+        .then((result) => cocktailDataToIdList(result))
+        .then((result2) => combinedIdLists.push(...result2))
+        .catch((error) => {
+          setErrorMessage('검색 결과가 없습니다')
+        })
     }
 
     if (filtering.category !== '') {
       filterKindCount += 1
 
-      const filteredByCategoryData = await getApiData(cocktailApis.filterByCategory, filtering.category)
-      const filteredCategoryIdList = cocktailDataToIdList(filteredByCategoryData)
-      combinedIdLists.push(...filteredCategoryIdList)
+      await getApiData(cocktailApis.filterByCategory, filtering.category)
+        .then((result) => cocktailDataToIdList(result))
+        .then((result2) => combinedIdLists.push(...result2))
+        .catch((error) => {
+          setErrorMessage('검색 결과가 없습니다')
+        })
     }
 
     if (filtering.ingredient !== '') {
       filterKindCount += 1
 
-      const filteredByIngredientData = await getApiData(cocktailApis.filterByIngredients, filtering.ingredient)
-      const filteredIngredientIdList = cocktailDataToIdList(filteredByIngredientData)
-      combinedIdLists.push(...filteredIngredientIdList)
+      await getApiData(cocktailApis.filterByIngredients, filtering.ingredient)
+        .then((result) => cocktailDataToIdList(result))
+        .then((result2) => combinedIdLists.push(...result2))
+        .catch((error) => {
+          setErrorMessage('검색 결과가 없습니다')
+        })
     }
 
-    /* const combinedIdLists = [
-      ...filteredNameIdList,
-      ...filteredAlcoholicIdList,
-      ...filteredCategoryIdList,
-      ...filteredIngredientIdList,
-    ] */
-    console.log(combinedIdLists)
-
-    // setTotalFilteredIdList(eliminateSameItem(combinedIdLists, filterKindCount))
+    try {
+      const dd = eliminateSameItem(combinedIdLists, filterKindCount)
+      console.log(combinedIdLists)
+      setTotalFilteredIdList(dd)
+    } catch {
+      setErrorMessage('검색어를 입력해주세요')
+    }
   }
 
   useEffect(() => {
-    const dd = (d: string) => {
-      const data = getApiData(cocktailApis.searchById, d)
-      return data
-    }
-    const aa = totalFilteredIdList.map((id) => {
-      const a = dd(id)
-      return a
+    totalFilteredIdList.forEach((filteredId) => {
+      getApiData(cocktailApis.searchById, filteredId).then((res) =>
+        setTotalResult((prevResult) => [...prevResult, res.drinks])
+      )
     })
-    console.log(aa)
   }, [totalFilteredIdList])
+
+  useEffect(() => {
+    console.log(totalResult)
+  }, [totalResult])
+
+  useEffect(() => {
+    console.log(errorMessage)
+  }, [errorMessage])
 
   return (
     <div className={styles.container}>
@@ -116,6 +134,12 @@ const Search = () => {
           <FilterBox filterKind='category' filterList={categoryList} filterCase='single' />
 
           <FilterBox filterKind='ingredient' filterList={ingredientList} filterCase='multiple' />
+        </div>
+
+        <div>
+          {totalResult.map((gg, i) => {
+            return <div key={i}>{gg?.idDrink}</div>
+          })}
         </div>
       </main>
     </div>
