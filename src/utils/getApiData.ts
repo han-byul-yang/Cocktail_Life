5de51ff573e157f08f56d0004daddb495b096c3b
Han Byul <yahb9@naver.com>
2022-08-23 15:35:56 +0900

    Fix: api 불러오는 도중 발생한 오류 처리 수정

--- src/utils/getApiData.ts
+++ src/utils/getApiData.ts
@@ -1,12 +1,12 @@
 import axios, { AxiosResponse } from 'axios'
+import { ICocktailData, IFilteredCocktailData } from 'types/types'
 
 const getApiData = async (api: (params: string) => Promise<AxiosResponse>, params: string) => {
   try {
     const { data } = await api(params)
     return data
   } catch (error) {
-    if (axios.isAxiosError(error)) return error.message
-    return error
+    throw Error('데이터를 불러오는 과정 중 문제가 생겼습니다')
   }
 }
 
