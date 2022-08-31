c42fc97d6621b51e931be1bb4d7ed798eb995387
Han Byul <yahb9@naver.com>
2022-08-31 17:33:55 +0900

    Feat: GNB 컴포넌트 리스트 생성

--- src/components/GNB/index.tsx
+++ src/components/GNB/index.tsx
@@ -1,5 +1,27 @@
+import { NavLink } from 'react-router-dom'
+
+import styles from './gnb.module.scss'
+
 const GNB = () => {
-  return <nav>GNB</nav>
+  return (
+    <nav className={styles.gnb}>
+      <ul>
+        <NavLink
+          to='/search'
+          className={({ isActive }) =>
+            `${styles.navLink} + ${isActive ? styles.activatedLink : styles.nonActivatedLink}`
+          }
+        >
+          <li>SEARCH</li>
+        </NavLink>
+        <NavLink to='/' className={({ isActive }) => `${isActive ? styles.activatedLink : styles.nonActivatedLink}`}>
+          <li>POPULAR</li>
+        </NavLink>
+      </ul>
+    </nav>
+  )
 }
 
 export default GNB
+
+// Link, li 순서 확인하기
