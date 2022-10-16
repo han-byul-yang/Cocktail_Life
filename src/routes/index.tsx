3f92c931a7d8712b6478530d7c36f3e831ba550d
Han Byul <yahb9@naver.com>
2022-10-16 15:44:06 +0900

    Feat: queryClient defaultOptions 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,23 +4,31 @@ import { QueryClientProvider, QueryClient } from 'react-query'
 
 import Popular from './Popular'
 import Search from './Search'
-import Result from './Result'
+import Detail from './Detail'
 import Layout from 'components/Layout'
 
 import styles from './routes.module.scss'
 
 const App = () => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 60,
+        refetchOnWindowFocus: false,
+        suspense: true,
+      },
+    },
+  })
 
   return (
     <div className={styles.background}>
       <RecoilRoot>
         <QueryClientProvider client={queryClient}>
           <Routes>
-            <Route path='/' element={<Popular />} />
             <Route element={<Layout />}>
-              <Route path='search' element={<Search />} />
-              <Route path='result' element={<Result />} />
+              <Route path='/' element={<Popular />} />
+              <Route path='/search' element={<Search />} />
+              <Route path='detail' element={<Detail />} />
             </Route>
             <Route path='*' element={<div>404</div>} />
           </Routes>
