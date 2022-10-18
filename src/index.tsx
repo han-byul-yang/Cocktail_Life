b599bcbf740d8d2b569457bd5177b374b282f308
Han Byul <yahb9@naver.com>
2022-10-18 18:28:21 +0900

    Feat: queryClient defaultOptions  설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
+import { RecoilRoot } from 'recoil'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import App from 'routes'
 
@@ -10,10 +13,24 @@ import './styles/index.scss'
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      // suspense: true,
+    },
+  },
+})
+
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>
 )
