a9295ae61ad8646e8d4693c0cdf5e246bd2b8d24
Han Byul <yahb9@naver.com>
2022-10-19 20:02:26 +0900

    Design: Search, Popular 페이지 반응형 화면 변환 시 cocktailCard 삐져나오는 문제 해결

--- src/routes/Popular/index.tsx
+++ src/routes/Popular/index.tsx
@@ -3,13 +3,19 @@ import { useQuery } from '@tanstack/react-query'
 import { popularCocktailApi } from 'services/getCocktailApis'
 import CocktailContainer from 'components/CocktailContainer'
 
+import styles from './popular.module.scss'
+
 const Popular = () => {
   const { isLoading, data: popularCocktailData } = useQuery(['popularCocktailApi'], popularCocktailApi, {
     retry: 1,
     select: (res) => res.data.drinks,
   })
 
-  return isLoading ? <div>isLoading.....</div> : <CocktailContainer resultData={popularCocktailData} type='popular' />
+  return (
+    <div className={styles.popularPage}>
+      {isLoading ? <div>isLoading.....</div> : <CocktailContainer resultData={popularCocktailData} type='popular' />}
+    </div>
+  )
 }
 
 export default Popular
