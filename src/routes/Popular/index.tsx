7c0f3ee8ede9273d76f2eb543769d9d94a4f3d1d
Han Byul <yahb9@naver.com>
2022-10-19 18:36:41 +0900

    Fix: Popular 페이지 캐로셀 삭제 및 Search 페이지와 같은 UI 제공

--- src/routes/Popular/index.tsx
+++ src/routes/Popular/index.tsx
@@ -1,76 +1,15 @@
-import { useState } from 'react'
-import { useQuery } from 'react-query'
-import { useRecoilState } from 'recoil'
-import { Link } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
 
-import { popularCocktailApi } from 'services/getApis'
-import { cocktailInitialData } from 'store/initialData/initialApiData'
-import { ICocktailData } from 'types/types'
-import { cocktailDataAtom } from 'store/atom'
-import Description from 'components/Description'
-
-import styles from './popular.module.scss'
+import { popularCocktailApi } from 'services/getCocktailApis'
+import CocktailContainer from 'components/CocktailContainer'
 
 const Popular = () => {
-  const [cocktailList, setCocktailList] = useRecoilState(cocktailDataAtom)
-  const [selectedRankBox, setSelectedRankBox] = useState(0)
-  // const [cocktailData, setCocktailData] = useState<ICocktailData[]>([cocktailInitialData])
+  const { isLoading, data: popularCocktailData } = useQuery(['popularCocktailApi'], popularCocktailApi, {
+    retry: 1,
+    select: (res) => res.data.drinks,
+  })
 
-  /* const { isFetching } = useQuery('popularCocktailApi', popularCocktailApi, {
-    onSuccess: (res) => {
-      setCocktailList(res.data.drinks)
-      console.log(res.data)
-    },
-  }) */
-
-  const handleRankBoxClick = (e: any) => {
-    setSelectedRankBox(Number(e.currentTarget.value))
-  }
-
-  return (
-    <>
-      <div className={styles.showBox}>
-        <ul className={styles.container} style={{ top: -(selectedRankBox * 800) }}>
-          {cocktailList.map((cocktailData, iList) => {
-            const { idDrink } = cocktailData
-
-            return (
-              <li key={idDrink} className={styles.cocktailRankBox}>
-                <Description cocktailData={cocktailData} iList={iList} />
-                <form className={styles.buttonForm}>
-                  {new Array(10).fill(undefined).map((ele, iRadio) => {
-                    const radioKey = `radio-${iRadio}`
-                    return (
-                      <input
-                        key={radioKey}
-                        type='radio'
-                        name='pageSelect'
-                        value={iRadio}
-                        id={`pageButton-${iRadio}`}
-                        onChange={handleRankBoxClick}
-                        checked={iRadio === selectedRankBox}
-                      />
-                    )
-                  })}
-                </form>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-      <Link to='search'>
-        <button type='button' className={styles.moveSearchPageButton}>
-          SEARCH COCKTAIL &gt;
-        </button>
-      </Link>
-    </>
-  )
+  return isLoading ? <div>isLoading.....</div> : <CocktailContainer resultData={popularCocktailData} type='popular' />
 }
 
 export default Popular
-
-// new Array(10).fill(undefinde) 한 이유
-// radio type
-// inital data 지저분
-// data split 10개 까지
-// form 태그 필요?
